ee2903483a85d12a961a89460ba6e2b5970bf99d
samuel <samuel.philipp@t-online.de>
2015-11-10 13:44:29 +0100

    Session 10.11.2015

--- src/main/java/de/saphijaga/spoozer/web/controller/UserController.java
+++ src/main/java/de/saphijaga/spoozer/web/controller/UserController.java
@@ -1,7 +1,9 @@
 package de.saphijaga.spoozer.web.controller;
 
+import de.saphijaga.spoozer.core.service.UserService;
 import de.saphijaga.spoozer.web.details.UserDetails;
 import de.saphijaga.spoozer.web.domain.response.GetUserDetailsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @MessageMapping("/getUserDetails")
     @SendTo("/setUserDetails")
     public GetUserDetailsResponse getUserDetails(UserDetails user) {
         return new GetUserDetailsResponse(user);
+    }
+
+    @MessageMapping("/saveUserDetails")
+    @SendTo("/setUserDetails")
+    public GetUserDetailsResponse saveUserDetails(UserDetails user) {
+        // TODO save UserDetails
+        return getUserDetails(user);
     }
 }
